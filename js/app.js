0deabb4a1b730dc504afffd892242fcde6646ea1
manu <manu.villar.vitoria@gmail.com>
2020-05-09 18:38:58 +0200

    reading the form data

--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
 const eventbrite = new EventBrite();
 const ui = new Interfaz();
+
+//Listener al buscador
+document.getElementById('buscarBtn').addEventListener('click', (e) => {
+    e.preventDefault();
+    //console.log('presionado');
+
+    //Leer el texto del input buscar
+    const textoBuscador = document.getElementById('evento').value;
+
+    //Leer el select
+    const categorias = document.getElementById('listado-categorias');
+    const categoriaSeleccionada = categorias.options[categorias.selectedIndex].value;
+
+    //console.log(textoBuscador);
+    //console.log(categoriaSeleccionada);
+
+    //Revisar que hay aalgo escrito en el buscador
+    if(textoBuscador != '') {
+        console.log('Buscando...');
+    } else {
+        console.log('No hay nada');
+    }
+})
